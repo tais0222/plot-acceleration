0c41a71933f73eeee7f9ae9059c5946240806627
tais0222 <129871696+tais0222@users.noreply.github.com>
2023-05-09 11:47:33 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,19 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(0)
+    basic.showNumber(max / (0 - 0))
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Yes)
 })
 input.onButtonPressed(Button.B, function () {
     max = 0
 })
+let aktuel = 0
 let max = 0
 max = 0
 input.setAccelerometerRange(AcceleratorRange.EightG)
 basic.forever(function () {
-	
+    aktuel = input.acceleration(Dimension.X)
+    if (aktuel > max) {
+        max = aktuel
+    }
 })
